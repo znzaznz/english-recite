3de03df7416ef1243b048240930962bf365fdb71
xiao <779740337@qq.com>
2021-04-10 19:33:19 +0800

    first commit

---
+++ src/views/WordTest.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function WordTest() {
+    return (
+        <div>
+            <header>
+                <img src="" alt="return"/>
+                <h2>词汇量测试</h2>
+            </header>
+            <main>
+                <div className="noReciteAccount">
+                    <span>当前您还有n个单词未复习</span>
+                    <div class="progress"></div>
+                </div>
+                <div className="showWord">word</div>
+                <section className={"isKnow"}>
+                    <button>认识</button>
+                    <button>不认识</button>
+                </section>
+            </main>
+        </div>
+    )
+}
