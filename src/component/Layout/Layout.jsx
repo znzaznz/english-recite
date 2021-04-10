6157a6e1906eebc4804355653fbd7cfccff51250
xiao <779740337@qq.com>
2021-04-10 21:31:08 +0800

    resolve error

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import "./index.scss"
 import ReturnNavButton from "../../views/WordTest/ReturnButtonNav/ReturnNavButton";
-import NoReciteAccount from "../../views/WordTest/NoReciteAccount/NoReciteAccount";
-import IsKnow from "../../views/WordTest/IsKnow/IsKnow";
 
 export default function Layout(props) {
     //这个组件用来做页面布局
--- src/component/SvgIcon/SvgIcon.jsx
+++ src/component/SvgIcon/SvgIcon.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import "./index.scss"
 
 export default function SvgIcon(props) {
-    console.log(props.isBack);
     return (
         <svg className={`icon ${props.className}`} aria-hidden={"true"}>
             {props.isBack ? <use xlinkHref={`#icon-${props.name}`}/> : <use xlinkHref={""}/>}
